d9260b47c2d9e8bdb3aa2671158a0d2e110ca8c5
Ryan <ryan.d.elliott@gmail.com>
2017-10-28 14:33:10 -0600

    ListingDetail basic formatting

--- src/components/ListingDetail.js
+++ src/components/ListingDetail.js
@@ -1,38 +1,48 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import './ListingDetail.css';
-import listingsJSON from '../db/data.json';
 import { fetchListing } from '../actions/listingsActions.js';
 
 const ListingDetail = props => {
 
     fetchListing();
 
-    let postDate = "1475028136";
-    let postDateString = `${(new Date(postDate*1000)).toLocaleDateString()}`;
+    let postDateEpoch = "1475028136";
+    let postDate = `${(new Date(postDateEpoch*1000)).toLocaleDateString()}`;
     let name = "Flovent HFA";
     let desc = "tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt pede ac urna. Ut tincidunt vehicula risus. Nulla eget metus eu erat semper rutrum.";
     let img = "http://wwwimage1.cbsstatic.com/thumbnails/videos/w848/CBS_Production_Entertainment_VMS/2017/09/28/1056668739786/CBS_COLBERT_0414_CLIP7_IMAGE_1222700_640x360.jpg";
     let qty = 20;
-    let categories = {categories};
+    let categories = "seafood";
     let location = "66.89462, -108.10672";
-    let pickUpDate = "1475500243";
+    let pickUpDateEpoch = "1475500243";
+    let pickUpDate = `${(new Date(pickUpDateEpoch*1000)).toLocaleDateString()}`;
     let haves = false;
     let available = "23:00";
     let email = "nec.enim.Nunc@ultricies.org";
 
 
     return(
-        <div> 
-            <h1>{name}</h1>
-            <h2>{postDateString}</h2>
-            <h2>{desc}</h2>
-            <h2>{img}</h2>
-            <h2>{location}</h2>
-            <h2>{email}</h2>
-            <h2>{pickUpDate}</h2>
-            <h2>{haves}</h2>
-            <h2>{available}</h2>
+        <div className="detail-container"> 
+            <header>             
+                <h1>{name}</h1>
+                <h2>Posted: {postDate}</h2>
+            </header>
+            <div className="description">
+                <img alt="item preview" className="summaryImg" src={img}/>
+                <h3>{desc}</h3>
+            </div>
+            <div className="description">
+                <h3>Categories: {categories}</h3>
+                <h3>Quantity: {qty}</h3>
+            </div>
+            <div className="description">
+                <h3>Location: {location}</h3>
+                <h3>Contact: {email}</h3>
+            </div>
+            <div className="description">
+                <h3>Pick Up Date: {pickUpDate}</h3>
+                <h3>Time Available: {available}</h3>
+            </div>
         </div>
     )
 }
