9eb1a48a7ef84d62e8ccf130c053061379b7988b
Ryan <ryan.d.elliott@gmail.com>
2017-10-28 12:37:02 -0600

    Refactor listings props

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Listing.css';
 
-const Listing = ({name, qty, desc, postDate, img, detailRoute}) => (
+const Listing = (props) => (
   <div className="listing-container">
   	<header>
-    	<h3>{name}</h3>
-    	<h5>Posted: {postDate}</h5>
+    	<h3>{props.name}</h3>
+    	<h5>Posted: {props.postDate}</h5>
     </header>
     <section>
-    	<img alt="item preview" className="summaryImg" src={img}/>
-   		<p>{desc}</p>
+    	<img alt="item preview" className="summaryImg" src={props.img}/>
+   		<p>{props.desc}</p>
 	</section>
 	<footer>
-		<p>Qty: {qty}</p>
-		<a href={detailRoute}>More Info</a>
+		<p>Qty: {props.qty}</p>
+		<a href={props.detailRoute}>More Info</a>
 	</footer>
   </div>
 )
